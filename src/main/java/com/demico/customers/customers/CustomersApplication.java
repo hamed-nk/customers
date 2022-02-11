61a0a6ddd5375a312745a670e0203496842322b2
i-laptop <hamed.dev70@gmail.com>
2022-02-11 19:05:04 +0330

    up21

--- src/main/java/com/demico/customers/customers/CustomersApplication.java
+++ src/main/java/com/demico/customers/customers/CustomersApplication.java
@@ -2,10 +2,12 @@ package com.demico.customers.customers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CustomersApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(CustomersApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CustomersApplication.class, args);
+    }
 }
