142e31d033063cfff03e3973f92c751e689d81a7
i-laptop <hamed.dev70@gmail.com>
2022-02-11 09:14:50 +0330

    up1

--- src/main/java/com/demico/customers/customers/controller/HomeController.java
+++ src/main/java/com/demico/customers/customers/controller/HomeController.java
@@ -1,11 +1,14 @@
 package com.demico.customers.customers.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/")
 public class HomeController {
+
+    @GetMapping
     public String getName(){
         return "my name is hamed";
     }
